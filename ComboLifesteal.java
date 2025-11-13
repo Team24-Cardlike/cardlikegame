f0ae06c74fb93aab7aa2d618b328049782ad1844
Viktor <viktormoden@yahoo.se>
2025-11-13 15:51:03 +0100

    Small change in combolifesteal

--- ComboLifesteal.java
+++ ComboLifesteal.java
@@ -1,9 +1,9 @@
 public class ComboLifesteal extends Upgrade{
     final Combo combo;
-    final int vampAmount;
-    ComboLifesteal(String name, Combo combo, int vampAmount, int cost) {
+    final double vampAmount;
+    ComboLifesteal(String name, Combo combo, double vampAmount, int cost) {
         super(name,
-                "Gain " + vampAmount + " lifesteal on " + combo.name + " combos.",
+                "Gain " + vampAmount + "% lifesteal on " + combo.name + " combos.",
                 cost, "Sustain");
         this.combo = combo;
         this.vampAmount = vampAmount;
