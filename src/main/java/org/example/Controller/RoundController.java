7df0e0aa3075e2161d06ab4226aac0df7f16c2c8
Nummer1InfFan <123357877+Nummer1InfFan@users.noreply.github.com>
2025-12-17 11:04:34 +0100

    Upgrade merge

--- src/main/java/org/example/Controller/RoundController.java
+++ src/main/java/org/example/Controller/RoundController.java
@@ -19,28 +19,22 @@ public class RoundController {
 
     public void nextRound(){
         round.endRound();
-
     }
 
     public void selectCard(int a) {
         round.addSelectedCards(a);
-
-
     }
 
     public void unselectCard(int a) {
         round.unselectCard(a);
-
-
     }
+
     public void discardCards(ArrayList<Integer> cards){
         this.round.discard();
-
     }
 
     public void playCards() {
         this.round.playCards();
-
     }
 
     public void openHandbook(){manager.setHandbookState();}
